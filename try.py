d74a5d73ebfa56924084a72a281633fc778a89b5
Itzyoreevss <kurosawarei05@gmail.com>
2025-09-03 02:00:51 +0800

    Add files via upload

---
+++ try.py
@@ -0,0 +1,14 @@
+numbers = [12, 35,  9, 56, 24, 88, 71, 3, 10, 42]
+
+even = []
+odd = []
+
+for number in numbers:
+    if  number % 2 == 0:
+        even.append(number)
+    else:
+        odd.append(number)
+    
+print(list)
+print(even)
+print(odd)
